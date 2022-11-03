836a6de996fb2fe55870154f4ce9b86871e55c4c
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-11-03 17:38:03 +1300

    Fixed urls and added pages

---
+++ copyflix/src/pages/Browse.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Browse extends Component {
+  render() {
+    return (
+      <div>
+        <h2 style={{ color: "white"}}> This should display the video cards</h2>
+        <p style={{ color: "white"}}>TODO: Add in movie cards</p>
+      </div>
+    );
+  }
+} 
+export default Browse;
