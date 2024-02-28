00ce9a7db39c2ff42de1ab13d7a918c7a8bdf912
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2024-02-28 15:56:30 +1300

    added skeleton load. Still needs work

---
+++ copyflix/src/pages/TVShows.js
@@ -0,0 +1,83 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
+
+const data = [
+  {
+    src: "https://i.ytimg.com/vi/pLqipJNItIo/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBkklsyaw9FxDmMKapyBYCn9tbPNQ",
+    title: "Don Diablo @ Tomorrowland Main Stage 2019 | Official…",
+    channel: "Don Diablo",
+    views: "396k views",
+    createdAt: "a week ago",
+  },
+  {
+    src: "https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA",
+    title: "Queen - Greatest Hits",
+    channel: "Queen Official",
+    views: "40M views",
+    createdAt: "3 years ago",
+  },
+  {
+    src: "https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw",
+    title: "Calvin Harris, Sam Smith - Promises (Official Video)",
+    channel: "Calvin Harris",
+    views: "130M views",
+    createdAt: "10 months ago",
+  },
+];
+
+function Media(props) {
+  const { loading = false } = props;
+  const [loaded, setLoading] = React.useState(false);
+  const counter = React.useRef(0);
+  const imageLoad = () => {
+    setLoading(true);
+  };
+
+  const imageLoaded = () => {
+    counter.current += 1;
+    if (counter.current >= data.length) {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Grid container wrap="nowrap">
+      {(loaded ? Array.from(new Array(3)) : data).map((item, index) => (
+        <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+          {item ? (
+            <img
+              style={{ width: 210, height: 118 }}
+              alt={item.title}
+              src={item.src}
+              onLoad={imageLoaded}
+            />
+          ) : (
+            <Skeleton
+              sx={{ bgcolor: "grey.900" }}
+              variant="rectangular"
+              width={210}
+              height={118}
+            />
+          )}
+        </Box>
+      ))}
+    </Grid>
+  );
+}
+
+Media.propTypes = {
+  loading: PropTypes.bool,
+};
+
+export default function TVShows() {
+  return (
+    <Box sx={{ overflow: "hidden" }}>
+      <Media loading />
+      <Media />
+    </Box>
+  );
+}
